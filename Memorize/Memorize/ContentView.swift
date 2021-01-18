8be95a4775dea30b023bf2b984a3caf706b01bde
yanleclerc <ea091041@ens.uqam.ca>
2021-01-17 19:31:01 -0500

    First Upload

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Yan-Alexandre Leclerc on 2021-01-16.
+//  Copyright © 2021 Yan-Alexandre Leclerc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0 ..< 4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+       
+    }
+}
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke( lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
